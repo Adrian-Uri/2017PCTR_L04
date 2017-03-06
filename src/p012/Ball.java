3ac4ca2b88676f07d6512e2a91a152fd0960852d
turbolargoo <danieldelgadosan@hotmail.com>
2017-03-06 08:50:24 +0100

    Hemos encontrado la bola

--- src/p012/Ball.java
+++ src/p012/Ball.java
@@ -5,7 +5,7 @@ import javax.swing.ImageIcon;
 
 //TODO Transform the code to be used safely in a concurrent context.  
 public class Ball {
-	// TODO Find an archive named Ball.png
+	
 	private String Ball = "pelota.png";
 
 	private double x, y, dx, dy;
